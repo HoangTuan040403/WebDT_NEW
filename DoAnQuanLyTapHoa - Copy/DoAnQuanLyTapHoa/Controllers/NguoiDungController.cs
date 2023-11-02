7472f2af87bd2086b1ea2fe37e438d8e70982d71
HuynhTrQuyenLam <iiam.qlam@gmail.com>
2023-11-02 22:43:57 +0700

    Dang nhap, dang ky ben nguoi dung

---
+++ DoAnQuanLyTapHoa - Copy/DoAnQuanLyTapHoa/Controllers/NguoiDungController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoAnQuanLyTapHoa.Models;
+using System.Globalization;
+
+namespace DoAnQuanLyTapHoa.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        QLBANDTEntities database = new QLBANDTEntities();
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangKy(User kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.TenUser))
+                    ModelState.AddModelError(string.Empty, "Họ tên không được để trống");
+                if (string.IsNullOrEmpty(kh.TenDN))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.MatKhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(kh.email))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(kh.sdt))
+                    ModelState.AddModelError(string.Empty, "Số điện thoại không được để trống");
+                if (string.IsNullOrEmpty(kh.DiaChi))
+                    ModelState.AddModelError(string.Empty, "Địa chỉ thoại không được để trống");
+                //
+                var khachhang = database.Users.FirstOrDefault(k => k.TenDN == kh.TenDN);
+                if (khachhang != null)
+                    ModelState.AddModelError(string.Empty, "Đã có người đăng ký tên này");
+                else if (ModelState.IsValid)
+                {
+                    database.Users.Add(kh);
+                    database.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(User kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.TenDN))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.MatKhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (ModelState.IsValid)
+                {
+                    var khachhang = database.Users.FirstOrDefault(k => k.TenDN == kh.TenDN && k.MatKhau == kh.MatKhau);
+                    if (khachhang != null)
+                    {
+                        ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                        Session["TaiKhoan"] = khachhang;
+                        Session["TenDN"] = khachhang.TenUser;
+                        Session["MaUser"] = khachhang.MaUser;
+
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+
+            return View();
+
+        }
+    }
+}
